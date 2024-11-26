19240b7b98bf1d3865008d178cdaa1970ebed739
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-11-26 04:06:06 -0600

    navbar version 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
                 <span className="navbar-title">Snickers</span>
             </div>
             <div className="navbar-right">
-                <a className="navbar-link">Excluusivos</a>
+                <a className="navbar-link">Exclusivos</a>
                 <a className="navbar-link">Nosotros</a>
                 <a className="navbar-link">Contacto</a>
                 <a className="navbar-link">Contacto</a>
